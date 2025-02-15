a286728f120c2bb580466de94cfeb1add2a1c8a2
ferrofy <team.ferrofy@gmail.com>
2025-02-16 02:05:42 +0530

    Create index.js

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+document.getElementById('Go_To_Main_Logs').addEventListener('click', function() {
+  window.location.href = 'https://ferrofy.github.io/Private_Data/Logs';
+});
+document.getElementById('Go_To_Past_Investments').addEventListener('click', function() {
+  window.location.href = 'https://ferrofy.github.io/Private_Data/Exchange_Logs';
+});
