77b3c301e73618498584bbbc906bff44f0697723
ferrofy <team.ferrofy@gmail.com>
2025-02-16 18:46:37 +0530

    Create 0xt.js

---
+++ Plan/JS/Farm/0xt.js
@@ -0,0 +1,134 @@
+const passwords = ["team_ferrofy", "vpx_login", "tanav_login","member_login" ];
+const Text_To_Display = " >>> Start Plan 'Farm 0xt' : 16 Feb 2025 [br] Investments - {VPX:725 , Tanav:725 , PPX:500 , AKX:80 , Angel:70} [br] Shares in % - {VPX:35.4 , Tanav:35.4 , PPX:22.5 , AKX:3.6 , Angel:3.1} [br] Finish [hr] [br] Any Error? Contact At team.ferrofy@gmail.com or vpx.ferrofy@gmail.com [br] It's Last Line Of Programm Thank You User For Your Time. Nothing Further :) [hr]";
+let speed = 10;
+let wrongPasswordSpeed = 10;
+let wrongPasswordAttempts = 0;
+let lengthErrorAttempts = 0;
+const maxPasswordAttempts = 5;
+const maxLengthAttempts = 3;
+const maxPasswordLength = 30;
+
+document.getElementById("password").addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    startLog();
+  } else {
+    if (event.target.value.length >= maxPasswordLength) {
+      event.preventDefault();
+      showLengthError();
+    }
+  }
+});
+
+function startLog() {
+  const passwordInput = document.getElementById("password").value.toLowerCase();
+  if (passwordInput.length > maxPasswordLength) {
+    showLengthError();
+    return;
+  }
+
+  if (passwords.includes(passwordInput)) {
+    document.querySelector(".password-container").style.display = "none";
+    displayText();
+  } else {
+    wrongPasswordAttempts++;
+    showError(passwordInput);
+    if (wrongPasswordAttempts >= maxPasswordAttempts) {
+      window.location.href = 'https://ferrofy.github.io/Home';
+    }
+  }
+}
+
+function displayText() {
+  const log = document.getElementById("log");
+  log.innerHTML = "";
+  let i = 0;
+
+  function typeNextChar() {
+    if (i < Text_To_Display.length) {
+      if (Text_To_Display.substring(i, i + 4) === "[hr]") {
+        log.innerHTML += ' <br> <hr class="neon-hr"> <br> ';
+        i += 4;
+      } else if (Text_To_Display.substring(i, i + 4) === "[br]") {
+        log.innerHTML += '<br> >>> ';
+        i += 4;
+      } else {
+        log.innerHTML += Text_To_Display.charAt(i);
+        i++;
+      }
+      setTimeout(typeNextChar, speed);
+    }
+  }
+
+  typeNextChar();
+}
+
+function showError(passwordInput) {
+  const attemptsLeft = maxPasswordAttempts - wrongPasswordAttempts;
+  const errorMessage = `Dear User, You Entered "${passwordInput}" Which Is Wrong. Please Retry.[br]Attempts Left: ${attemptsLeft}[br]Thank You For Your Time`;
+  displayOverlayMessage(errorMessage, wrongPasswordSpeed);
+}
+
+function showLengthError() {
+  lengthErrorAttempts++;
+  const attemptsLeft = maxLengthAttempts - lengthErrorAttempts;
+  const errorMessage = `Dear User, You Entered Password More Than 30 Characters Which Is Not Allowed.[br]Attempts Left: ${attemptsLeft}[br]Thank You For Your Time`;
+  displayOverlayMessage(errorMessage, wrongPasswordSpeed);
+  if (lengthErrorAttempts >= maxLengthAttempts) {
+    window.location.href = 'https://ferrofy.github.io/Home';
+  }
+}
+
+function displayOverlayMessage(message, speed) {
+  let errorIndex = 0;
+  const overlay = document.createElement('div');
+  overlay.style.position = 'fixed';
+  overlay.style.top = 0;
+  overlay.style.left = 0;
+  overlay.style.width = '100%';
+  overlay.style.height = '100%';
+  overlay.style.backgroundColor = '#121212';
+  overlay.style.color = '#00FF00';
+  overlay.style.display = 'flex';
+  overlay.style.flexDirection = 'column';
+  overlay.style.justifyContent = 'center';
+  overlay.style.alignItems = 'center';
+  overlay.style.fontSize = '24px';
+  overlay.style.textAlign = 'center';
+  overlay.style.zIndex = 1000;
+  overlay.innerHTML = "";
+  document.body.appendChild(overlay);
+
+  function typeMessage() {
+    if (errorIndex < message.length) {
+      if (message.substring(errorIndex, errorIndex + 4) === "[br]") {
+        overlay.innerHTML += '<br>';
+        errorIndex += 4;
+      } else {
+        overlay.innerHTML += message.charAt(errorIndex);
+        errorIndex++;
+      }
+      setTimeout(typeMessage, speed);
+    } else {
+      overlay.addEventListener("click", () => {
+        document.body.removeChild(overlay);
+      });
+      setTimeout(() => {
+        if (document.body.contains(overlay)) {
+          document.body.removeChild(overlay);
+        }
+      }, 3000);
+    }
+  }
+
+  typeMessage();
+}
+
+function togglePasswordVisibility() {
+  const passwordInput = document.getElementById("password");
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
+  }
+}
